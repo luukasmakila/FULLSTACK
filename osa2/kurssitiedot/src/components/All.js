65251fdbf5ad7382e834d4a9f7a568be37446f37
Luukas <luukas.makila@kolumbus.fi>
2021-12-25 18:36:53 +0200

    2.5 valmis

---
+++ osa2/kurssitiedot/src/components/All.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const All = ({value}) => {
+    return (
+        <b>total of {value} exercises</b>
+    )
+}
+
+export default All
--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -1,9 +1,13 @@
 import React from "react"
 import Header from "./Header"
 import Content from "./Content"
+import All from "./All"
 
 const Course = ({course}) => {
     const parts = course.parts
+    const exercises = parts.map(part => part.exercises)
+    const all = exercises.reduce((a,b) => a + b, 0)
+
     return (
         <div>
             <Header header={course.name}/>
@@ -11,6 +15,7 @@ const Course = ({course}) => {
                 {parts.map(part =>
                     <Content key={part.id} part={part}/>
                 )}
+            <All value={all}/>
             </div>
         </div>
     )
