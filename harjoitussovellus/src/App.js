54f55277b2ed2be4e87106ccbc55b1e5bcac951e
Luukas <luukas.makila@kolumbus.fi>
2021-12-25 23:07:21 +0200

    puhelinluettelo step6 teht 2.11

--- harjoitussovellus/src/App.js
+++ harjoitussovellus/src/App.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from "axios"
 import Note from './components/Note'
 
-const App = ({notes}) => {
+const App = () => {
+
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState("")
+  const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log("effect")
+    axios
+    .get("http://localhost:3001/notes")
+    .then(response => {
+      console.log("promise fullfilled") 
+      setNotes(response.data)
+    })
+  }
+  useEffect(hook, [])
+  
+  console.log("render", notes.length, "notes")
 
   return (
     <div>
@@ -13,6 +31,7 @@ const App = ({notes}) => {
       </ul>
     </div>
   )
+
 }
 
 export default App
