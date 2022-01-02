3ca393733e402ad46c55f9e1997145d06aad8fca
Luukas <luukas.makila@kolumbus.fi>
2022-01-02 18:21:35 +0200

    4.13 ja 4.14

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,12 +1,38 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (request, response) => {
-    Blog
-    .find({})
-    .then(blogs => {
+blogRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
     response.json(blogs)
-    })
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+    const id = request.params.id
+    try {
+        await Blog.findByIdAndDelete(id)
+        response.status(200).end()
+    } catch(exception) {
+        response.status(400).end()
+    }
+})
+
+blogRouter.put('/:id', async (request, response) => {
+    const id = request.params.id
+    const body = request.body
+
+    const updatedBlog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+
+    try{
+        await Blog.findByIdAndUpdate(id, updatedBlog, {new: true})
+        response.status(200).end()
+    } catch(exception) {
+        response.status(400).end()
+    }
 })
   
 blogRouter.post('/', async (request, response) => {
